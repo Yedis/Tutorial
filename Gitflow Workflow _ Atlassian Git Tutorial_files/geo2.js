8d13c02f32711f28d18bd6b81a140f04e32407c6
Yedis <Yedis_hd@outlook.com>
2019-04-08 11:54:14 +0800

    update

---
+++ Gitflow Workflow _ Atlassian Git Tutorial_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "HANGZHOU",
+      'continent': "AS",
+      'country': "CN",
+      'region': "ZJ"
+    },
+    'ip':"210.32.7.115"
+  }]);
+})
+//
+()
+
+;
